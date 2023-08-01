822ca32c47b25d0065cca7e8a3b561dac28c5a85
Rocio Altamirano <rocio.altamirano@smfconsulting.es>
2023-08-01 09:37:54 -0300

    Feature EML-328: tool configuration

--- copilot/core/routes.py
+++ copilot/core/routes.py
@@ -1,7 +1,7 @@
 """Routes for the core blueprint."""
 from dotenv import dotenv_values
 from flask import render_template, request
-
+import json
 # pylint: disable=import-error, no-name-in-module
 from transformers.tools import (
     OpenAiAgent,
@@ -12,6 +12,26 @@ from .bastian_tool import BastianFetcher, XMLTranslatorTool
 import os
 
 OPENAI_API_KEY= os.getenv("OPENAI_API_KEY")
+
+class ToolManager:
+    def __init__(self, tool_config_path="tools_config.json"):
+        self.tools = []
+        self.tool_config_path = tool_config_path
+        self.load_tools()
+        
+    def load_tools(self):
+        with open(self.tool_config_path, 'r') as file:
+            tool_config = json.load(file)
+
+        if tool_config.get("BastianFetcher", "enabled") == "enabled":
+            self.tools.append(BastianFetcher())
+
+        if tool_config.get("XMLTranslatorTool", "enabled") == "enabled":
+            self.tools.append(XMLTranslatorTool())
+        
+    def get_tools(self):
+        return self.tools
+
 
 @core.route("/", methods=["GET"])
 def serve_index():
@@ -28,17 +48,15 @@ def serve_question():
     """Serves the question answering endpoint."""
     # Get the JSON data from the request
     data = request.get_json()
-
+    tool_manager = ToolManager()
+    enabled_tools = tool_manager.get_tools()
     # Extract the question from the data
     question = data["question"]
-
-    bastian_tool = BastianFetcher()
-    translator_tool = XMLTranslatorTool()
 
     agent = OpenAiAgent(
         model="gpt-4",
         api_key=OPENAI_API_KEY,
-        additional_tools=[bastian_tool,translator_tool],
+        additional_tools=enabled_tools,
     )
             
     response = agent.chat(
@@ -46,7 +64,9 @@ def serve_question():
         remote=True,
         return_code=False,
     )
-    
+    if isinstance(response, list):
+        response = "\n".join(response)
+
     if isinstance(response, (dict, str)):
         return {
             "answer": response,
