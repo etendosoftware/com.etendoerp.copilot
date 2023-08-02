8259968094ece67a0012be6b2b6832888a6f5d69
Franco Buffa <fgbuffa@gmail.com>
2023-08-02 16:57:37 -0300

    EML-336: skip import issue for now

--- copilot/core/__init__.py
+++ copilot/core/__init__.py
@@ -3,9 +3,12 @@ import logging
 
 from flask import Blueprint, render_template
 
+core = Blueprint("core", __name__)
+
+# TODO: EML-346
+# fmt: off
 # pylint: disable=wrong-import-position
 # This pylint error is disabled because the routes module needs to import the
 # blueprint defined in this module.
-from . import routes
-
-core = Blueprint("core", __name__)
+from . import routes  # noqa:E402
+# fmt: on
