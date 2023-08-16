69e412e80d2baa9d74a90fc2446b149a4577ed03
Franco Buffa <fgbuffa@gmail.com>
2023-08-16 18:23:29 -0300

    EML-355: add unit test cases for tool manager and fix styling

--- copilot/core/tool_manager.py
+++ copilot/core/tool_manager.py
@@ -2,7 +2,15 @@ import json
 from typing import Dict, Final, List
 
 # tools need to be imported so they can be inferred from globals
+# ruff: noqa: F401
+# fmt: off
+from .bastian_tool import BastianFetcher
+from .retrieval_tool import RetrievalTool
 from .tool_wrapper import ToolWrapper
+from .xml_translator_tool import XMLTranslatorTool
+
+# fmt: on
+
 
 CONFIGURED_TOOLS_FILENAME: Final[str] = "tools_config.json"
 
@@ -10,10 +18,15 @@ CONFIGURED_TOOLS_FILENAME: Final[str] = "tools_config.json"
 def load_configured_tools(config_filename: str = CONFIGURED_TOOLS_FILENAME) -> List[ToolWrapper]:
     configured_tools: List[ToolWrapper] = []
     with open(config_filename, "r") as config_tool_file:
-        tool_config: Dict = json.load(config_tool_file)
+        try:
+            tool_config: Dict = json.load(config_tool_file)
+        except json.decoder.JSONDecodeError as ex:
+            raise Exception("Unsupported tool configuration file format") from ex
 
         for tool_implementation in ToolWrapper.__subclasses__():
-            if tool_implementation.__name__ in tool_config:
+            if tool_implementation.__name__ in tool_config and tool_config.get(
+                tool_implementation.__name__, False
+            ):
                 class_name = globals()[tool_implementation.__name__]
                 configured_tools.append(class_name())
 
---
+++ tests/test_tool_manager.py
@@ -0,0 +1,45 @@
+import json
+
+import pytest
+from copilot.core.bastian_tool import BastianFetcher
+from copilot.core.tool_manager import load_configured_tools
+
+
+@pytest.fixture
+def empty_config_file():
+    temp_file = "/tmp/temp_file.txt"
+    with open(temp_file, "w") as f:
+        f.write("")
+    yield temp_file
+
+
+@pytest.fixture
+def fake_json_config_file():
+    json_file_path = "/tmp/temp_file.json"
+    with open(json_file_path, "w") as json_file:
+        json.dump({"Tools": "Enabled"}, json_file, indent=4)
+    yield json_file_path
+
+
+@pytest.fixture
+def fake_valid_config_file():
+    json_file_path = "/tmp/temp_file.json"
+    with open(json_file_path, "w") as json_file:
+        json.dump({"BastianFetcher": True, "XMLTranslatorTool": False}, json_file, indent=4)
+    yield json_file_path
+
+
+def test_load_configured_tools_empty_file(empty_config_file):
+    with pytest.raises(Exception) as exc_info:
+        load_configured_tools(config_filename=empty_config_file)
+    assert str(exc_info.value) == "Unsupported tool configuration file format"
+
+
+def test_load_configured_tools_no_valid_config(fake_json_config_file):
+    assert load_configured_tools(config_filename=fake_json_config_file) == []
+
+
+def test_load_configured_tools_with_valid_file(fake_valid_config_file):
+    configured_tools = load_configured_tools(config_filename=fake_valid_config_file)
+    assert len(configured_tools) == 1
+    assert isinstance(configured_tools[0], BastianFetcher)
