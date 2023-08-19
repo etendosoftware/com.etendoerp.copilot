4e5bddce12659b22637300c3dd73740fb69348df
Franco Buffa <fgbuffa@gmail.com>
2023-08-19 16:46:53 -0300

    Remove multiprocessing and Flask creation refactor

--- copilot/app.py
+++ copilot/app.py
@@ -1,45 +1,36 @@
 """Initialize Flask app."""
-import os
+from typing import Optional
 
-from flask import Flask, render_template
+from flask import Flask
 
-# Register blueprints
 from .core import core_blueprint
 
 
-class Copilot:
-    """The Copilot app."""
+def create_app(self, test_config: Optional[str] = None) -> Flask:
+    """Create and configure an instance of the Flask application.
 
-    def run(self):
-        """Run the app."""
-        self.create_app().run(debug=True, host="0.0.0.0", port=os.getenv("COPILOT_PORT"))
+    Args:
 
-    def create_app(self, test_config=None):
-        """Create and configure an instance of the Flask application.
+    Returns:
+        Flask: The Flask app.
+    """
+    app: Flask = Flask(__name__, instance_relative_config=True)
 
-        Args:
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-        Returns:
-            Flask: The Flask app.
-        """
-        app = Flask(__name__, instance_relative_config=True)
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return "not found", 404
 
-        if test_config is None:
-            # load the instance config, if it exists, when not testing
-            app.config.from_pyfile("config.py", silent=True)
-        else:
-            # load the test config if passed in
-            app.config.from_mapping(test_config)
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return "interal server error", 500
 
-        @app.errorhandler(404)
-        def page_not_found(error):
-            # note that we set the 404 status explicitly
-            return render_template("404.html"), 404
+    app.register_blueprint(core_blueprint)
 
-        @app.errorhandler(500)
-        def internal_server_error(error):
-            return render_template("500.html"), 500
-
-        app.register_blueprint(core_blueprint)
-
-        return app
+    return app
