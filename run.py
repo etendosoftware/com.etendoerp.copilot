29bf301948aba8cafa45af286e9eb4631ded7eca
Franco Buffa <fgbuffa@gmail.com>
2023-09-06 09:28:16 -0300

    EML-338: fix uvicorn execution

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ import uvicorn
 from copilot import app
 
 if __name__ == "__main__":
-    uvicorn.run(app.app, host="localhost", port=int(os.getenv("COPILOT_PORT")))
+    uvicorn.run(app.app, host="0.0.0.0", port=int(os.getenv("COPILOT_PORT")))
