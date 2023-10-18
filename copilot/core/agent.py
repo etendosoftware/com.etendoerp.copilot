44a1b0fd1fd4a4c8bf7080c483a14e1037282e90
Rocio Altamirano <rocio.altamirano@smfconsulting.es>
2023-10-18 15:02:08 -0300

    Feature EE-732: change in hardcoded variable

--- copilot/core/agent.py
+++ copilot/core/agent.py
@@ -7,10 +7,11 @@ from .tool_manager import configured_tools
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+MODEL = os.getenv("OPENAI_MODEL", "gpt-3.5-turbo")
 
 def create_open_ai_agent() -> OpenAiAgent:
     return OpenAiAgent(
-        model="gpt-4",
+        model=MODEL,
         api_key=OPENAI_API_KEY,
         additional_tools=configured_tools,
     )
