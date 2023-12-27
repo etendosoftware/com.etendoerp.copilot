449fd18ead97ac329bb5c789522df933287f1d2d
Tomás Rodeghiero <tomyrodeghiero@gmail.com>
2023-12-27 12:05:14 -0300

    Feature EML-425: Improve scroll to bottom effect

--- web/com.etendoerp.copilot.chat/src/components/CodeComponent.tsx
+++ web/com.etendoerp.copilot.chat/src/components/CodeComponent.tsx
@@ -1,6 +1,5 @@
 import { nightOwl } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { useState } from "react";
 
 export const CodeComponent = ({
     node,
@@ -9,27 +8,9 @@ export const CodeComponent = ({
     children,
     ...props
 }: any) => {
-    const [copiedCode, setCopiedCode] = useState(false);
-
-    // Function to copy the code
-    const handleCopyCode = (code: string) => {
-        navigator.clipboard.writeText(code);
-        setCopiedCode(true);
-    };
-
     if (!inline) {
         return (
             <div className="my-2 w-full rounded">
-                <div className="font-regular flex items-center justify-end rounded-t-[0.5rem] bg-[#343444] px-2 text-[0.75rem]">
-                    <div
-                        className="flex cursor-pointer items-center gap-2"
-                        onClick={() => handleCopyCode(children)}
-                    >
-                        <p className="text-white-900 text-xs py-1">
-                            {copiedCode ? "¡Texto copiado!" : "Copiar código"}
-                        </p>
-                    </div>
-                </div>
                 <SyntaxHighlighter
                     children={String(children).replace(/\n$/, "")}
                     style={nightOwl}
@@ -37,8 +18,7 @@ export const CodeComponent = ({
                     PreTag="div"
                     wrapLongLines={true}
                     customStyle={{
-                        borderBottomRightRadius: "0.5rem",
-                        borderBottomLeftRadius: "0.5rem",
+                        borderRadius: "0.25rem",
                         fontSize: "0.75rem",
                     }}
                     {...props}
