d9940f83125c6265e389bd06f61ec971c147a454
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2024-05-27 13:04:28 -0300

    Feature EML-500: Error for never Synched Assistants.

--- src/com/etendoerp/copilot/rest/RestServiceUtil.java
+++ src/com/etendoerp/copilot/rest/RestServiceUtil.java
@@ -225,9 +225,14 @@ public class RestServiceUtil {
     if (copilotApp == null) {
       throw new OBException(String.format(OBMessageUtils.messageBD("ETCOP_AppNotFound"), appId));
     }
-    List<String> qId = new ArrayList<>();
-    qId.add(questionAttachedFileId);
-    return handleQuestion(copilotApp, conversationId, question, qId);
+    if (StringUtils.equalsIgnoreCase(copilotApp.getAppType(), CopilotConstants.APP_TYPE_OPENAI)
+        && StringUtils.isEmpty(copilotApp.getOpenaiIdAssistant())) {
+      throw new OBException(String.format(OBMessageUtils.messageBD("ETCOP_OpenAIAppNotSync"), appId));
+    }
+
+    List<String> filesReceived = new ArrayList<>();
+    filesReceived.add(questionAttachedFileId); // File path in temp folder. This files were attached in the pop-up.
+    return handleQuestion(copilotApp, conversationId, question, filesReceived);
   }
 
   public static JSONObject handleQuestion(CopilotApp copilotApp, String conversationId, String question,
