303ce8a5f32914ad7be6f2d0c841bccec52e88f5
Javier Bertero <javier.bertero@smfconsulting.es>
2024-07-31 10:22:43 -0300

    Feature EML-605: Adding thee sync of KB langchain agent

--- src/com/etendoerp/copilot/util/CopilotUtils.java
+++ src/com/etendoerp/copilot/util/CopilotUtils.java
@@ -6,32 +6,54 @@ import static com.etendoerp.copilot.util.CopilotConstants.PROVIDER_OPENAI;
 import static com.etendoerp.copilot.util.CopilotConstants.PROVIDER_OPENAI_VALUE;
 import static com.etendoerp.copilot.util.OpenAIUtils.HEADER_CONTENT_TYPE;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
+import org.hibernate.criterion.Restrictions;
 import org.openbravo.base.exception.OBException;
 import org.openbravo.base.session.OBPropertiesProvider;
+import org.openbravo.base.weld.WeldUtils;
+import org.openbravo.client.application.attachment.AttachImplementationManager;
 import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBCriteria;
+import org.openbravo.dal.service.OBDal;
 import org.openbravo.erpCommon.businessUtility.Preferences;
 import org.openbravo.erpCommon.utility.OBMessageUtils;
+import org.openbravo.model.ad.utility.Attachment;
 
 import com.etendoerp.copilot.data.CopilotApp;
+import com.etendoerp.copilot.data.CopilotAppSource;
+import com.etendoerp.copilot.data.CopilotFile;
+import com.etendoerp.copilot.hook.CopilotFileHookManager;
 
 public class CopilotUtils {
 
 
   public static final HashMap<String, String> PROVIDER_MAP_CODE_NAME = buildProviderCodeMap();
   public static final HashMap<String, String> PROVIDER_MAP_CODE_DEFAULT_PROP = buildProviderCodeDefaulMap();
+
+  private static final Logger log = LogManager.getLogger(OpenAIUtils.class);
 
   private static HashMap<String, String> buildProviderCodeMap() {
     HashMap<String, String> map = new HashMap<>();
@@ -176,5 +198,112 @@ public class CopilotUtils {
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  private static boolean fileHasChanged(CopilotFile fileToSync) {
+
+    Date lastSyncDate = fileToSync.getLastSync();
+    if (lastSyncDate == null) {
+      return true;
+    }
+    Date updated = fileToSync.getUpdated();
+    //clean the milliseconds
+    lastSyncDate = new Date(lastSyncDate.getTime() / 1000 * 1000);
+    updated = new Date(updated.getTime() / 1000 * 1000);
+
+    if ((updated.after(lastSyncDate))) {
+      return true;
+    }
+    //check Attachments
+    OBCriteria<Attachment> attCrit = OBDal.getInstance().createCriteria(Attachment.class);
+    attCrit.add(Restrictions.eq(Attachment.PROPERTY_RECORD, fileToSync.getId()));
+    Attachment attach = (Attachment) attCrit.setMaxResults(1).uniqueResult();
+    if (attach == null) {
+      return false;
+    }
+
+    Date updatedAtt = attach.getUpdated();
+    updatedAtt = new Date(updatedAtt.getTime() / 1000 * 1000);
+    return updatedAtt.after(lastSyncDate);
+  }
+
+  public static void logIfDebug(String text) {
+    if (log.isDebugEnabled()) {
+      log.debug(text);
+    }
+  }
+
+  public static File getFileFromCopilotFile(CopilotFile fileToSync) throws IOException {
+    AttachImplementationManager aim = WeldUtils.getInstanceFromStaticBeanManager(
+        AttachImplementationManager.class);
+    OBCriteria<Attachment> attCrit = OBDal.getInstance().createCriteria(Attachment.class);
+    attCrit.add(Restrictions.eq(Attachment.PROPERTY_RECORD, fileToSync.getId()));
+    Attachment attach = (Attachment) attCrit.setMaxResults(1).uniqueResult();
+    if (attach == null) {
+      throw new OBException(String.format(OBMessageUtils.messageBD("ETCOP_ErrorMissingAttach"),
+          fileToSync.getName()));
+    }
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    aim.download(attach.getId(), os);
+    //save os to temp file
+    //create a temp file
+    String filename = attach.getName();
+    String fileWithoutExtension = filename.substring(0, filename.lastIndexOf("."));
+    String extension = filename.substring(filename.lastIndexOf(".") + 1);
+    File tempFile = File.createTempFile(fileWithoutExtension, "." + extension);
+    boolean setW = tempFile.setWritable(true);
+    if (!setW) {
+      logIfDebug("The temp file is not writable");
+    }
+    tempFile.deleteOnExit();
+    os.writeTo(new FileOutputStream(tempFile));
+    return tempFile;
+  }
+
+  private static String readFileToSync(CopilotFile fileToSync)
+      throws IOException {
+    File tempFile = getFileFromCopilotFile(fileToSync);
+    StringBuilder text = new StringBuilder();
+    try (FileInputStream fis = new FileInputStream(tempFile);
+         InputStreamReader isr = new InputStreamReader(fis);
+         BufferedReader br = new BufferedReader(isr)) {
+
+      String line;
+      while ((line = br.readLine()) != null) {
+        text.append(line).append("\n");
+      }
+    }
+    return text.toString();
+  }
+
+  public static void syncAppLangchainSource(CopilotAppSource appSource)
+      throws IOException {
+
+    CopilotFile fileToSync = appSource.getFile();
+    WeldUtils.getInstanceFromStaticBeanManager(CopilotFileHookManager.class)
+        .executeHooks(fileToSync);
+    /*if (!fileHasChanged(fileToSync)) {
+      logIfDebug("File " + fileToSync.getName() + " not has changed, skipping sync");
+      return;
+    }*/
+
+    /*if (StringUtils.isNotEmpty(fileToSync.getIdFile())) {
+      //we will delete the file
+      logIfDebug("Deleting file " + fileToSync.getName());
+      deleteFile(fileToSync.getOpenaiIdFile(), openaiApiKey);
+    }
+    String fileId = OpenAIUtils.downloadAttachmentAndUploadFile(fileToSync, openaiApiKey);
+    fileToSync.setOpenaiIdFile(fileId);*/
+
+    logIfDebug("Uploading file " + fileToSync.getName());
+    String text = readFileToSync(fileToSync);
+    String dbName = "KB_" + appSource.getEtcopApp().getId();
+
+    textToChroma(text, dbName);
+
+    fileToSync.setLastSync(new Date());
+    fileToSync.setUpdated(new Date());
+    OBDal.getInstance().save(fileToSync);
+    OBDal.getInstance().flush();
   }
 }
