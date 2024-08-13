96666e4f79cb9d9020efc97a669f1fba0428d5a7
sebastianbarrozo <sebastian.barrozo@etendo.software>
2024-08-13 18:22:29 +0200

    Feature EML-614: added Javadocs

--- src/com/etendoerp/copilot/rest/CopilotJwtServlet.java
+++ src/com/etendoerp/copilot/rest/CopilotJwtServlet.java
@@ -12,10 +12,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * This servlet has two main responsibilities: 1) authenticate, 2) set the
+ * correct {@link OBContext} , and 3) translate Exceptions into the correct Http
+ * response code.
+ * <p>
+ * In regard to authentication: there is support for basic-authentication as
+ * well as url parameter based authentication.
+ * </p>
+ */
 public class CopilotJwtServlet extends HttpBaseServlet {
   private static final Logger log4j = LogManager.getLogger(CopilotJwtServlet.class);
   private static RestService instance;
 
+  /**
+   * Get the instance of the RestService
+   *
+   * @return
+   */
   public static RestService getInstance() {
     if (instance == null) {
       instance = new RestService();
@@ -23,6 +37,12 @@ public class CopilotJwtServlet extends HttpBaseServlet {
     return instance;
   }
 
+  /**
+   * Obtain the token from the request
+   *
+   * @param request
+   * @return
+   */
   private String obtainToken(HttpServletRequest request) {
     String authStr = request.getHeader("Authorization");
     String token = null;
@@ -32,6 +52,12 @@ public class CopilotJwtServlet extends HttpBaseServlet {
     return token;
   }
 
+  /**
+   * Check the JWT token
+   *
+   * @param request
+   * @throws Exception
+   */
   private void checkJwt(HttpServletRequest request) throws Exception {
     DecodedJWT decodedToken = SecureWebServicesUtils.decodeToken(obtainToken(request));
     String userId = decodedToken.getClaim("user").asString();
@@ -48,6 +74,11 @@ public class CopilotJwtServlet extends HttpBaseServlet {
     OBContext.setOBContextInSession(request, OBContext.getOBContext());
   }
 
+  /**
+   * Handle the GET request
+   *
+   * @param request
+   */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     try {
@@ -69,6 +100,17 @@ public class CopilotJwtServlet extends HttpBaseServlet {
     }
   }
 
+  /**
+   * Handle the POST request
+   *
+   * @param request  an {@link HttpServletRequest} object that
+   *                 contains the request the client has made
+   *                 of the servlet
+   * @param response an {@link HttpServletResponse} object that
+   *                 contains the response the servlet sends
+   *                 to the client
+   * @throws IOException
+   */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     try {
--- src/com/etendoerp/copilot/rest/CopilotSecureServlet.java
+++ src/com/etendoerp/copilot/rest/CopilotSecureServlet.java
@@ -6,25 +6,59 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * This servlet has two main responsibilities: 1) authenticate, 2) set the
+ * correct {@link OBContext} , and 3) translate Exceptions into the correct Http
+ * response code.
+ * <p>
+ * In regard to authentication: there is support for basic-authentication as
+ * well as url parameter based authentication.
+ * </p>
+ */
 public class CopilotSecureServlet extends HttpSecureAppServlet {
   private static RestService instance;
 
+  /**
+   * Get the instance of the RestService
+   *
+   * @return
+   */
   public static RestService getInstance() {
-    if(instance == null) {
+    if (instance == null) {
       instance = new RestService();
     }
     return instance;
   }
 
+  /**
+   * Obtain the token from the request
+   *
+   * @param request  an {@link HttpServletRequest} object that
+   *                 contains the request the client has made
+   *                 of the servlet
+   * @param response an {@link HttpServletResponse} object that
+   *                 contains the response the servlet sends
+   *                 to the client
+   * @throws IOException
+   */
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     getInstance().doGet(request, response);
   }
 
+  /**
+   * Obtain the token from the request
+   *
+   * @param request  an {@link HttpServletRequest} object that
+   *                 contains the request the client has made
+   *                 of the servlet
+   * @param response an {@link HttpServletResponse} object that
+   *                 contains the response the servlet sends
+   *                 to the client
+   * @throws IOException
+   */
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     getInstance().doPost(request, response);
   }
 
