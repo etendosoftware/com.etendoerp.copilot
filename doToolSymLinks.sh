0ca2de24b9c6fb9360016ab89a502169c4b34d31
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2024-08-20 09:08:22 -0300

    Feature EML-616: Updated doSymLinksScript

--- doToolSymLinks.sh
+++ doToolSymLinks.sh
@@ -16,75 +16,71 @@ for arg in "$@"; do
 done
 
 echo "Starting script to create symbolic links for tools and tests"
+
+# Navegar al main_module y limpiar los enlaces simbólicos en tools y tests
+cd "$main_module"
+
+echo "Cleaning symbolic links in tools and tests folders"
 cd tools
-#delete all the symbolic links in the tools folder
+find . -type l -delete
+cd ../tests
 find . -type l -delete
 cd ..
 
-# Get the list of submodules, which are at the same level as the main module
-for module in ../*; do # for example ../com.etendoerp.copilot.erp
-    # Check if the submodule has a "tools" folder
-    echo "Checking submodule: $module"
-    if [ -d "$module/tools" ]; then # if the folder com.etendoerp.copilot.erp/tools exists
-        echo "Submodule with tools found: $module"
-        
-        # Iterate over the files within the "tools" folder of the submodule
-        for tool in "$module/tools"/*; do # for example ../com.etendoerp.copilot.erp/tools/Ejemplo.py
-            # Get the filename without the path
-            tool_name=$(basename "$tool") # tool_name = Ejemplo.py
-            echo "Checking tool: $tool_name"
-            echo "tool: $tool"
-            # Check if the file already exists in the main module
-            if [ ! -e "tools/$tool_name" ] && [[ "$tool_name" == *.py ]]; then # if the file com.etendoerp.copilot/tools/Ejemplo.py does not exist and the file extension is .py
-                # Create the symbolic link, com.etendoerp.copilot/tools/Ejemplo.py -> com.etendoerp.copilot.erp/tools/Ejemplo.py
-                echo "Creating symbolic link for $tool_name, command is: ln -s $tool tools/$tool_name"
-                
-                
-                
-                ln -s "../$tool" "tools/$tool_name" 
-                echo "Symbolic link created for $tool_name"
-            elif [ -L "tools/$tool_name" ]; then
-                # Replace the existing symbolic link,  com.etendoerp.copilot/tools/Ejemplo.py -> com.etendoerp.copilot.erp/tools/Ejemplo.py
-                ln -sf "../$tool" "tools/$tool_name" 
-                echo "Symbolic link updated for $tool_name"
-            else
-                echo "The file $tool_name already exists in the main module."
+# Volver al directorio de trabajo original para iterar sobre los submódulos
+cd ..
+echo "Going back to the original working directory"
+echo "Current directory: $(pwd)"
+
+
+# Iterar sobre los submódulos, excluyendo el main_module
+for module in ./*; do
+    # Saltar el main_module
+    if [[ "$module" == "$main_module" ]]; then
+        echo "Skipping main module: $module"
+        continue
+    fi
+
+    # Crear enlaces simbólicos en la carpeta tools del main_module
+    if [ -d "$module/tools" ]; then
+        echo "Creating symbolic links for tools from $module"
+        for tool in "$module/tools"/*; do
+            tool_name=$(basename "$tool")
+            if [ ! -e "$main_module/tools/$tool_name" ] && [[ "$tool_name" == *.py ]]; then
+                ln -s "../../$tool" "$main_module/tools/$tool_name"
+                echo "Symbolic link created for $tool_name in tools"
+            elif [ -L "$main_module/tools/$tool_name" ]; then
+                ln -sf "../../$tool" "$main_module/tools/$tool_name"
+                echo "Symbolic link updated for $tool_name in tools"
             fi
         done
     fi
-    # delete all the symbolic links in the tests folder
-    cd tests
-    find . -type l -delete
-    cd ..
-    # Check if the submodule has a "tests" folder
-    if [ -d "$module/tests/" ]; then # if the folder com.etendoerp.copilot.erp/tools exists
-        for _test in "$module/tests"/*; do # for example ../com.etendoerp.copilot.erp/tools/Ejemplo.py
-            # Get the filename without the path
-            test_name=$(basename "$_test") # test_name = Ejemplo.py
-            echo "Checking _test: $test_name"
-            echo "_test: $_test"
-            # Check if the file already exists in the main module
-            if [ ! -e "tests/$test_name" ] && [[ "$test_name" == *.py ]]; then # if the file com.etendoerp.copilot/tools/Ejemplo.py does not exist and the file extension is .py
-                # Create the symbolic link, com.etendoerp.copilot/tools/Ejemplo.py -> com.etendoerp.copilot.erp/tools/Ejemplo.py
-                echo "Creating symbolic link for $test_name, command is: ln -s $_test tests/$test_name"
-                ln -s "../$_test" "tests/$test_name"
-                echo "Symbolic link created for $test_name"
-            else
-                echo "The file $test_name already exists in the main module."
+
+    # Crear enlaces simbólicos en la carpeta tests del main_module
+    if [ -d "$module/tests" ]; then
+        echo "Creating symbolic links for tests from $module"
+        for _test in "$module/tests"/*; do
+            test_name=$(basename "$_test")
+            if [ ! -e "$main_module/tests/$test_name" ] && [[ "$test_name" == *.py ]]; then
+                ln -s "../../$_test" "$main_module/tests/$test_name"
+                echo "Symbolic link created for $test_name in tests"
+            elif [ -L "$main_module/tests/$test_name" ]; then
+                ln -sf "../../$_test" "$main_module/tests/$test_name"
+                echo "Symbolic link updated for $test_name in tests"
             fi
         done
         if [ -d "$module/tests/resources" ]; then
-            echo "Copying resources folder"
-            cp -r "$module/tests/resources" "tests/"
+            echo "Copying resources folder from $module"
+            cp -r "$module/tests/resources" "$main_module/tests/"
         fi
     fi
 done
 
-
-# come back to the main module
+# Volver a la carpeta del main_module
 echo "Going back to the main module"
-cd ../$main_module
-#the tools_config file is created in the main module, the tools_config file is used to store the tools that are going to be used in the main module
+cd "$main_module"
+
+# Ejecutar el script local_setup.py si no se pasó la opción --no-local-setup
 if [ "$no_local_setup" = false ]; then
     echo "Running local_setup.py"
     exec python3 local_setup.py
