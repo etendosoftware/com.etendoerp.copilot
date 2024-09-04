4daef02392e069680f2c34695c2845149e076031
Javier Bertero <javier.bertero@smfconsulting.es>
2024-09-04 12:32:35 -0300

    Feature ETP-253: Add javadoc to KBSyncStatusHandler

--- src/com/etendoerp/copilot/eventhandler/KBSyncStatusHandler.java
+++ src/com/etendoerp/copilot/eventhandler/KBSyncStatusHandler.java
@@ -22,23 +22,37 @@ import com.etendoerp.copilot.data.CopilotTool;
 import com.etendoerp.copilot.util.CopilotConstants;
 import com.etendoerp.copilot.util.CopilotUtils;
 
-
+/**
+ * Handles synchronization status updates for the Knowledge Base (KB) in response to entity events.
+ */
 public class KBSyncStatusHandler extends EntityPersistenceEventObserver {
+
   private static Entity[] entities = {
       ModelProvider.getInstance().getEntity(CopilotFile.class) };
   protected Logger logger = Logger.getLogger(KBSyncStatusHandler.class);
 
+  /**
+   * Returns the list of entities observed by this handler.
+   *
+   * @return An array of {@link Entity} objects that this handler observes.
+   */
   @Override
   protected Entity[] getObservedEntities() {
     return entities;
   }
 
+  /**
+   * Handles the entity update event. Checks if any relevant properties of the {@link CopilotFile} entity
+   * have changed, and if so, updates the synchronization status of the associated application.
+   *
+   * @param event The {@link EntityUpdateEvent} to be handled.
+   */
   public void onUpdate(@Observes EntityUpdateEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
     final CopilotFile currentFile = (CopilotFile) event.getTargetInstance();
-    
+
     if (checkFilePropertiesChanged(event, currentFile.getEntity())) {
       updateAppSyncStatus(currentFile);
     }
@@ -46,12 +60,23 @@ public class KBSyncStatusHandler extends EntityPersistenceEventObserver {
     CopilotUtils.logIfDebug("The KB was updated");
   }
 
+  /**
+   * Handles the entity save event.
+   *
+   * @param event The {@link EntityNewEvent} to be handled.
+   */
   public void onSave(@Observes EntityNewEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
   }
 
+  /**
+   * Handles the entity delete event. Updates the synchronization status of the associated application
+   * when a {@link CopilotFile} is deleted.
+   *
+   * @param event The {@link EntityDeleteEvent} to be handled.
+   */
   public void onDelete(@Observes EntityDeleteEvent event) {
     if (!isValidEvent(event)) {
       return;
@@ -62,6 +87,13 @@ public class KBSyncStatusHandler extends EntityPersistenceEventObserver {
     CopilotUtils.logIfDebug("The KB was deleted");
   }
 
+  /**
+   * Updates the synchronization status of the associated application to "Pending Synchronization"
+   * when a {@link CopilotFile} is updated or deleted.
+   *
+   * @param currentFile The {@link CopilotFile} whose associated application's synchronization status
+   *                    will be updated.
+   */
   private static void updateAppSyncStatus(CopilotFile currentFile) {
     OBCriteria<CopilotAppSource> appSourceCrit = OBDal.getInstance().createCriteria(CopilotAppSource.class);
     appSourceCrit.add(Restrictions.eq(CopilotAppSource.PROPERTY_FILE, currentFile));
@@ -73,7 +105,14 @@ public class KBSyncStatusHandler extends EntityPersistenceEventObserver {
     }
   }
 
-
+  /**
+   * Checks if any of the specified properties of the {@link CopilotFile} entity have changed
+   * during an update event.
+   *
+   * @param event      The {@link EntityUpdateEvent} to be checked.
+   * @param fileEntity The {@link Entity} representing the {@link CopilotFile}.
+   * @return {@code true} if any of the specified properties have changed, {@code false} otherwise.
+   */
   private static boolean checkFilePropertiesChanged(EntityUpdateEvent event, Entity fileEntity) {
     String[] properties = {
         CopilotFile.PROPERTY_ORGANIZATION,
