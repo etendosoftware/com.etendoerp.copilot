e1156cf649db2bda29c834ac8b44a6d120aba082
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2024-09-27 11:42:13 -0300

    Issue #11: Pull request fixes
    ETP-251

--- src/com/etendoerp/copilot/eventhandler/SkillToolViewHandler.java
+++ src/com/etendoerp/copilot/eventhandler/SkillToolViewHandler.java
@@ -7,7 +7,6 @@ import org.apache.log4j.Logger;
 import org.openbravo.base.exception.OBException;
 import org.openbravo.base.model.Entity;
 import org.openbravo.base.model.ModelProvider;
-import org.openbravo.base.structure.BaseOBObject;
 import org.openbravo.client.kernel.event.EntityDeleteEvent;
 import org.openbravo.client.kernel.event.EntityNewEvent;
 import org.openbravo.client.kernel.event.EntityPersistenceEvent;
@@ -20,28 +19,51 @@ import org.openbravo.model.ad.system.Client;
 
 import com.etendoerp.copilot.data.CopilotAppTool;
 import com.etendoerp.copilot.data.TeamMember;
-import com.etendoerp.copilot.data.ToolWebhook;
 
-
+/**
+ * Event handler for SkillToolView.
+ * This class observes the persistence events (create, update, delete) for the CopilotAppTool and TeamMember entities
+ * and ensures that only the system administrator can perform these operations.
+ */
 public class SkillToolViewHandler extends EntityPersistenceEventObserver {
-  private static Entity[] entities = { ModelProvider.getInstance().getEntity(
+  private static final Entity[] entities = { ModelProvider.getInstance().getEntity(
       CopilotAppTool.class), ModelProvider.getInstance().getEntity(
       TeamMember.class) };
   protected Logger logger = Logger.getLogger(SkillToolViewHandler.class);
 
+  /**
+   * Returns the list of entities observed by this event handler.
+   *
+   * @return An array of observed entities.
+   */
   @Override
   protected Entity[] getObservedEntities() {
     return entities;
   }
 
+  /**
+   * Handles the update event for the observed entities.
+   * This method checks if the event is valid and then verifies the system administrator role.
+   *
+   * @param event
+   *     The entity update event.
+   */
   public void onUpdate(@Observes EntityUpdateEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
-    String clientID = getClientID(event);
-    checkSysAdminRole(clientID);
+
+    checkSameClient(event);
   }
 
+  /**
+   * Retrieves the client ID from the event.
+   * This method extracts the client ID from the target instance of the event.
+   *
+   * @param event
+   *     The entity persistence event.
+   * @return The client ID.
+   */
   private String getClientID(EntityPersistenceEvent event) {
     var obj = event.getTargetInstance();
     String clientID = null;
@@ -53,28 +75,51 @@ public class SkillToolViewHandler extends EntityPersistenceEventObserver {
     return clientID;
   }
 
+  /**
+   * Handles the save event for the observed entities.
+   * This method checks if the event is valid and then verifies the system administrator role.
+   *
+   * @param event
+   *     The entity new event.
+   */
   public void onSave(@Observes EntityNewEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
-    String clientID = getClientID(event);
-    checkSysAdminRole(clientID);
+
+    checkSameClient(event);
   }
 
+  /**
+   * Handles the delete event for the observed entities.
+   * This method checks if the event is valid and then verifies the system administrator role.
+   *
+   * @param event
+   *     The entity delete event.
+   */
   public void onDelete(@Observes EntityDeleteEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
-    String clientID = getClientID(event);
-    checkSysAdminRole(clientID);
+    checkSameClient(event);
   }
 
-  private void checkSysAdminRole(String clientID) {
+  /**
+   * Checks if the current user has the system administrator role.
+   * If the current user is not the system administrator, an OBException is thrown.
+   *
+   * @param event
+   *     The entity delete event.
+   * @throws OBException
+   *     If the current user is not the system administrator.
+   */
+  private void checkSameClient(EntityPersistenceEvent event) {
+    String clientID = getClientID(event);
     Client currentClient = OBContext.getOBContext().getCurrentClient();
     Client objClient = OBDal.getInstance().get(Client.class, clientID);
     if (objClient == null || !StringUtils.equalsIgnoreCase(currentClient.getId(), objClient.getId())) {
       String name = objClient == null ? "" : objClient.getName();
-      throw new OBException(String.format(OBMessageUtils.messageBD("ETCOP_errorClient"), name));
+      throw new OBException(OBMessageUtils.messageBD("ETCOP_errorClient"));
     }
   }
 }
--- src/com/etendoerp/copilot/eventhandler/ToolWebhookAccess.java
+++ src/com/etendoerp/copilot/eventhandler/ToolWebhookAccess.java
@@ -1,6 +1,5 @@
 package com.etendoerp.copilot.eventhandler;
 
-
 import javax.enterprise.event.Observes;
 
 import org.apache.commons.lang.StringUtils;
@@ -19,16 +18,32 @@ import org.openbravo.model.ad.system.Client;
 
 import com.etendoerp.copilot.data.ToolWebhook;
 
-
+/**
+ * Event handler for ToolWebhook entity.
+ * This class observes the persistence events (create, update, delete) for the ToolWebhook entity
+ * and ensures that only the system administrator can perform these operations.
+ */
 public class ToolWebhookAccess extends EntityPersistenceEventObserver {
   private static Entity[] entities = { ModelProvider.getInstance().getEntity(ToolWebhook.class) };
   protected Logger logger = Logger.getLogger(ToolWebhookAccess.class);
 
+  /**
+   * Returns the list of entities observed by this event handler.
+   *
+   * @return An array of observed entities.
+   */
   @Override
   protected Entity[] getObservedEntities() {
     return entities;
   }
 
+  /**
+   * Handles the update event for the ToolWebhook entity.
+   * This method checks if the event is valid and then verifies the system administrator role.
+   *
+   * @param event
+   *     The entity update event.
+   */
   public void onUpdate(@Observes EntityUpdateEvent event) {
     if (!isValidEvent(event)) {
       return;
@@ -36,6 +51,13 @@ public class ToolWebhookAccess extends EntityPersistenceEventObserver {
     checkSysAdminRole();
   }
 
+  /**
+   * Handles the save event for the ToolWebhook entity.
+   * This method checks if the event is valid and then verifies the system administrator role.
+   *
+   * @param event
+   *     The entity new event.
+   */
   public void onSave(@Observes EntityNewEvent event) {
     if (!isValidEvent(event)) {
       return;
@@ -43,14 +65,27 @@ public class ToolWebhookAccess extends EntityPersistenceEventObserver {
     checkSysAdminRole();
   }
 
+  /**
+   * Handles the delete event for the ToolWebhook entity.
+   * This method checks if the event is valid and then verifies the system administrator role.
+   *
+   * @param event
+   *     The entity delete event.
+   */
   public void onDelete(@Observes EntityDeleteEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
-
     checkSysAdminRole();
   }
 
+  /**
+   * Checks if the current user has the system administrator role.
+   * If the current user is not the system administrator, an OBException is thrown.
+   *
+   * @throws OBException
+   *     If the current user is not the system administrator.
+   */
   private void checkSysAdminRole() {
     Client currentClient = OBContext.getOBContext().getCurrentClient();
     Client sysClient = OBDal.getInstance().get(Client.class, "0");
