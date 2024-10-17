f0710699671a11f0d12d2db48f8f9359a0976f93
Javier Bertero <javier.bertero@smfconsulting.es>
2024-10-17 10:42:11 -0300

    Feature ETP-373: Change to async method in text to chroma

--- tests/test_text_to_chroma.py
+++ tests/test_text_to_chroma.py
@@ -3,49 +3,52 @@ import os
 import shutil
 import zipfile
 from dotenv import load_dotenv
+import pytest
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from httpx import AsyncClient
 
 from copilot.core.routes import core_router
-
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 load_dotenv(dotenv_path)
 
-client = TestClient(core_router)
+@pytest.mark.asyncio
+async def test_process_text_to_vector_db_zip_file():
+    app = FastAPI()
+    app.include_router(core_router)
 
-def test_process_text_to_vector_db_zip_file():
-    kb_vectordb_id = "test_kb_id"
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        kb_vectordb_id = "test_kb_id"
 
-    # If the directory exists, delete it
-    db_path = "./vectordbs/test_kb_id.db"
-    if os.path.exists(db_path):
-        shutil.rmtree(db_path)
+        # If the directory exists, delete it
+        db_path = "./vectordbs/test_kb_id.db"
+        if os.path.exists(db_path):
+            shutil.rmtree(db_path)
 
-    # Set the input data
-    filename = "test.zip"
-    extension = "zip"
-    overwrite = True
+        # Set the input data
+        filename = "test.zip"
+        extension = "zip"
+        overwrite = True
 
-    # Create a ZIP file in memory
-    zip_buffer = io.BytesIO()
-    with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
-        zip_file.writestr("dummy.txt", "Dummy content inside zip file")
-    zip_buffer.seek(0)
+        # Create a ZIP file in memory
+        zip_buffer = io.BytesIO()
+        with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
+            zip_file.writestr("dummy.txt", "Dummy content inside zip file")
+        zip_buffer.seek(0)
 
-    # Simulate the upload of a ZIP file
-    response = client.post(
-        "/addToVectorDB",
-        data={
-            "kb_vectordb_id": kb_vectordb_id,
-            "filename": filename,
-            "extension": extension,
-            "overwrite": str(overwrite).lower()  # Convert to string
-        },
-        files={"file": (filename, zip_buffer, "application/zip")}
-    )
+        # Simulate the upload of a ZIP file
+        response = await client.post(
+            "/addToVectorDB",
+            data={
+                "kb_vectordb_id": kb_vectordb_id,
+                "filename": filename,
+                "extension": extension,
+                "overwrite": str(overwrite).lower()
+            },
+            files={"file": (filename, zip_buffer, "application/zip")}
+        )
 
-    # Check a successful response
-    assert response.status_code == 200
-    assert response.json()["success"] is True
-
-
+        # Check a successful response
+        assert response.status_code == 200
+        assert response.json()["success"] is True
