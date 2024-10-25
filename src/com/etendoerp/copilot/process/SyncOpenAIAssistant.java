4006f3f2c988cc55499966249147325b6cbeaa98
Francisco Roig <francisco.roig@smfconsulting.es>
2024-10-25 13:27:20 -0300

    Feature ETP-198: Sync logic improved for modularity and extensibility

--- src/com/etendoerp/copilot/process/SyncOpenAIAssistant.java
+++ src/com/etendoerp/copilot/process/SyncOpenAIAssistant.java
@@ -1,12 +1,10 @@
 package com.etendoerp.copilot.process;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -45,96 +43,18 @@ public class SyncOpenAIAssistant extends BaseProcessActionHandler {
 
   @Override
   protected JSONObject doExecute(Map<String, Object> parameters, String content) {
-
     // Declare json to be returned
     JSONObject result = new JSONObject();
     try {
       OBContext.setAdminMode();
       // Get request parameters
       JSONObject request = new JSONObject(content);
-      JSONArray selecterRecords = request.optJSONArray("recordIds");
-      int totalRecords = (selecterRecords == null) ? 0 : selecterRecords.length();
-      if (totalRecords == 0) {
-        throw new OBException(OBMessageUtils.messageBD("ETCOP_NoSelectedRecords"));
-      }
-      int syncCount = 0;
-      String openaiApiKey = OpenAIUtils.getOpenaiApiKey();
-
-      List<CopilotApp> appList = new ArrayList<>();
-
-      for (int i = 0; i < totalRecords; i++) {
-        CopilotApp app = OBDal.getInstance().get(CopilotApp.class, selecterRecords.getString(i));
-        if (app != null) { // Null check
-          appList.add(app);
-        }
-      }
-      Set<CopilotAppSource> appSourcesToRefresh = new HashSet<>();
-      Set<CopilotAppSource> appSourcesToSync = new HashSet<>();
-      for (CopilotApp app : appList) {
-        checkWebHookAccess(app);
-        List<CopilotAppSource> appSources = app.getETCOPAppSourceList();
-        List<CopilotAppSource> listSourcesForKb = appSources.stream().filter(CopilotConstants::isKbBehaviour).collect(
-            Collectors.toList());
-        app.setSyncStatus(CopilotConstants.SYNCHRONIZED_STATE);
-        if (StringUtils.equalsIgnoreCase(app.getAppType(), CopilotConstants.APP_TYPE_OPENAI)) {
-          if (!listSourcesForKb.isEmpty() && !app.isCodeInterpreter() && !app.isRetrieval()) {
-
-            throw new OBException(
-                String.format(OBMessageUtils.messageBD("ETCOP_Error_KnowledgeBaseIgnored"), app.getName()));
-          }
-        }
-        appSourcesToSync.addAll(listSourcesForKb);
-        appSourcesToRefresh.addAll(appSources);
-      }
-      for (CopilotAppSource as : appSourcesToRefresh) {
-        log.debug("Syncing file " + as.getFile().getName());
-        CopilotFile fileToSync = as.getFile();
-        WeldUtils.getInstanceFromStaticBeanManager(CopilotFileHookManager.class)
-            .executeHooks(fileToSync);
-      }
-
-      //for langchain apps we need to reset vector DB, so collect all langchain apps and reset the vector DBs
-      List<CopilotApp> langchainApps = appList.stream().filter(
-              app -> StringUtils.equalsIgnoreCase(app.getAppType(), CopilotConstants.APP_TYPE_LANGCHAIN))
-          .distinct().collect(Collectors.toList());
-      for (CopilotApp langApp : langchainApps) {
-        CopilotUtils.resetVectorDB(langApp);
-      }
-
-      if (!appList.isEmpty()) {
-        syncOpenaiModels(openaiApiKey);
-      }
-      for (CopilotAppSource appSource : appSourcesToSync) {
-
-        if (StringUtils.equalsIgnoreCase(appSource.getEtcopApp().getAppType(), CopilotConstants.APP_TYPE_OPENAI)) {
-
-          OpenAIUtils.syncAppSource(appSource, openaiApiKey);
-
-
-        } else if (StringUtils.equalsIgnoreCase(appSource.getEtcopApp().getAppType(),
-            CopilotConstants.APP_TYPE_LANGCHAIN)) {
-          CopilotUtils.syncAppLangchainSource(appSource);
-        } else {
-          // For langgraph apps, nothing to do
-        }
-      }
-      for (CopilotApp langApp : langchainApps) {
-        CopilotUtils.purgeVectorDB(langApp);
-      }
-
-      for (CopilotApp app : appList) {
-        if (StringUtils.equalsIgnoreCase(app.getAppType(), CopilotConstants.APP_TYPE_OPENAI)) {
-          OBDal.getInstance().refresh(app);
-          OpenAIUtils.refreshVectorDb(app);
-          OBDal.getInstance().refresh(app);
-          syncCount = callSync(syncCount, openaiApiKey, app);
-        } else {
-          syncCount++;
-        }
-      }
-
-
-      returnSuccessMsg(result, syncCount, totalRecords);
+      JSONArray selectedRecords = request.optJSONArray("recordIds");
+      List<CopilotApp> appList = getSelectedApps(selectedRecords);
+      // Generate attachment for each file
+      generateFilesAttachment(appList);
+      // Sync knowledge files to each assistant
+      result = syncKnowledgeFiles(appList);
     } catch (Exception e) {
       log.error("Error in process", e);
       try {
@@ -147,8 +67,8 @@ public class SyncOpenAIAssistant extends BaseProcessActionHandler {
         errorMessage.put("title", OBMessageUtils.messageBD("Error"));
         errorMessage.put("text", message);
         result.put("message", errorMessage);
-      } catch (Exception ignore) {
-        log.error("Error in process", ignore);
+      } catch (Exception ex) {
+        log.error("Error in process", ex);
       }
     } finally {
       OBContext.restorePreviousMode();
@@ -156,6 +76,66 @@ public class SyncOpenAIAssistant extends BaseProcessActionHandler {
     return result;
   }
 
+  /**
+   * This method retrieves a list of {@link CopilotApp} instances based on the given selected records.
+   * It processes the provided {@link JSONArray} of record IDs, fetching each corresponding
+   * {@link CopilotApp} from the database and adding it to the resulting list.
+   * If the input array is null or empty, an {@link OBException} is thrown to indicate that no
+   * records were selected. If a record ID does not correspond to a valid {@link CopilotApp},
+   * it is ignored and not added to the list.
+   *
+   * @param selectedRecords
+   *     The {@link JSONArray} containing the IDs of the selected records to be retrieved.
+   * @return
+   *     A list of {@link CopilotApp} objects corresponding to the provided record IDs.
+   * @throws JSONException
+   *     If an error occurs while processing the {@link JSONArray}.
+   * @throws OBException
+   *     If the {@link JSONArray} is null or contains no records.
+   */
+  private List<CopilotApp> getSelectedApps(JSONArray selectedRecords) throws JSONException {
+    List<CopilotApp> appList = new ArrayList<>();
+    if (selectedRecords == null || selectedRecords.length() == 0) {
+      throw new OBException(OBMessageUtils.messageBD("ETCOP_NoSelectedRecords"));
+    }
+
+    for (int i = 0; i < selectedRecords.length(); i++) {
+      CopilotApp app = OBDal.getInstance().get(CopilotApp.class, selectedRecords.getString(i));
+      if (app != null) {
+        appList.add(app);
+      }
+    }
+    return appList;
+  }
+
+  /**
+   * This method generates file attachments for a list of {@link CopilotApp} instances.
+   * It processes each application in the given list, verifying webhook access and retrieving
+   * the associated {@link CopilotAppSource} objects to be refreshed.
+   * The method updates the synchronization status of each {@link CopilotApp} to "synchronized"
+   * and collects all the related {@link CopilotAppSource} instances. It then processes each
+   * source by logging the file name and executing the relevant hooks using the
+   * {@link CopilotFileHookManager}.
+   *
+   * @param appList
+   *     A list of {@link CopilotApp} instances for which file attachments are being generated.
+   */
+  private void generateFilesAttachment(List<CopilotApp> appList) {
+    List<CopilotAppSource> appSourcesToRefresh = new ArrayList<>();
+    for (CopilotApp app : appList) {
+      checkWebHookAccess(app);
+      List<CopilotAppSource> appSources = app.getETCOPAppSourceList();
+      app.setSyncStatus(CopilotConstants.SYNCHRONIZED_STATE);
+      appSourcesToRefresh.addAll(appSources);
+    }
+
+    for (CopilotAppSource as : appSourcesToRefresh) {
+      log.debug("Syncing file {}", as.getFile().getName());
+      CopilotFile fileToSync = as.getFile();
+      WeldUtils.getInstanceFromStaticBeanManager(CopilotFileHookManager.class)
+              .executeHooks(fileToSync);
+    }
+  }
 
   /**
    * This method checks the access of each role to the webhooks associated with the given application.
@@ -193,7 +173,7 @@ public class SyncOpenAIAssistant extends BaseProcessActionHandler {
     Basically, we are looking for the roles that don't have access to the webhook, if the quantity of records is 0, the role doesn't have access
    */
     List<Object[]> results = OBDal.getInstance().getSession().createQuery(hql.toString())
-        .setParameter("appId", app.getId()).list();
+            .setParameter("appId", app.getId()).list();
     if (log.isDebugEnabled()) {
       log.debug(String.format("Results: %d", results.size()));
     }
@@ -251,6 +231,126 @@ public class SyncOpenAIAssistant extends BaseProcessActionHandler {
     OBDal.getInstance().save(newRole);
   }
 
+  /**
+   * This method synchronizes knowledge base files for a list of {@link CopilotApp} instances.
+   * It iterates over the provided list of applications, filtering the associated
+   * {@link CopilotAppSource} objects to include only those that represent knowledge base files.
+   * The synchronization is handled differently based on the application type.
+   * <p>Supported application types:
+   * <ul>
+   *   <li>{@link CopilotConstants#APP_TYPE_OPENAI}: Synchronizes files with the OpenAI API.</li>
+   *   <li>{@link CopilotConstants#APP_TYPE_LANGCHAIN}: Synchronizes files with the LangChain API.</li>
+   *   <li>{@link CopilotConstants#APP_TYPE_LANGGRAPH}: No synchronization is performed.</li>
+   *   <li>For other application types, an error is logged.</li>
+   * </ul>
+   *
+   * @param appList
+   *     A list of {@link CopilotApp} instances for which knowledge base files are being synchronized.
+   * @return
+   *     A {@link JSONObject} containing a message indicating the number of successfully
+   *     synchronized applications and the total number of applications processed.
+   * @throws JSONException
+   *     If an error occurs while building the result message.
+   * @throws IOException
+   *     If an input/output error occurs during synchronization.
+   */
+  private JSONObject syncKnowledgeFiles(List<CopilotApp> appList) throws JSONException, IOException {
+    int syncCount = 0;
+    String openaiApiKey = OpenAIUtils.getOpenaiApiKey();
+    // Update list of OpenAI models
+    syncOpenaiModels(openaiApiKey);
+    for (CopilotApp app : appList) {
+      List<CopilotAppSource> knowledgeBaseFiles = app.getETCOPAppSourceList().stream()
+              .filter(CopilotConstants::isKbBehaviour)
+              .toList();
+
+      switch (app.getAppType()) {
+        case CopilotConstants.APP_TYPE_OPENAI:
+          syncKBFilesToOpenAI(app, knowledgeBaseFiles, openaiApiKey);
+          break;
+        case CopilotConstants.APP_TYPE_LANGCHAIN:
+          syncKBFilesToLangChain(app, knowledgeBaseFiles);
+          break;
+        case CopilotConstants.APP_TYPE_LANGGRAPH:
+          log.debug("Sync not needed for LangGraph");
+          break;
+        default:
+          log.error("No handler found for API type: {}", app.getAppType());
+      }
+      syncCount++;
+    }
+    return buildMessage(syncCount, appList.size());
+  }
+
+  /**
+   * This method synchronizes knowledge base files with the OpenAI API for a given {@link CopilotApp}.
+   * It processes the list of provided {@link CopilotAppSource} instances, sending each one to
+   * the OpenAI API for synchronization. The method also performs additional steps to refresh
+   * the application's state and synchronize the assistant configuration.
+   *
+   * <p>If the application is configured for OpenAI but the knowledge base files are present,
+   * and it is neither a code interpreter nor retrieval-enabled, an {@link OBException} is thrown
+   * to indicate that the knowledge base files are ignored for the given app configuration.
+   *
+   * <p>After synchronizing each knowledge base file, the application's state is refreshed from
+   * the database, the vector database is updated, and the assistant configuration is synchronized
+   * with the OpenAI API.
+   *
+   * @param app
+   *     The {@link CopilotApp} instance for which knowledge base files are being synchronized.
+   * @param knowledgeBaseFiles
+   *     A list of {@link CopilotAppSource} objects representing the knowledge base files to be synchronized.
+   * @param openaiApiKey
+   *     The API key used for authentication with the OpenAI API.
+   * @throws JSONException
+   *     If an error occurs while processing JSON data.
+   * @throws IOException
+   *     If an input/output error occurs during the synchronization process.
+   * @throws OBException
+   *     If the application's configuration does not allow for knowledge base synchronization.
+   */
+  private void syncKBFilesToOpenAI(CopilotApp app, List<CopilotAppSource> knowledgeBaseFiles, String openaiApiKey) throws JSONException, IOException {
+    if (StringUtils.equalsIgnoreCase(app.getAppType(), CopilotConstants.APP_TYPE_OPENAI)) {
+      if (!knowledgeBaseFiles.isEmpty() && !app.isCodeInterpreter() && !app.isRetrieval()) {
+        throw new OBException(
+                String.format(OBMessageUtils.messageBD("ETCOP_Error_KnowledgeBaseIgnored"), app.getName()));
+      }
+    }
+    for (CopilotAppSource appSource : knowledgeBaseFiles) {
+      OpenAIUtils.syncAppSource(appSource, openaiApiKey);
+    }
+    OBDal.getInstance().refresh(app);
+    OpenAIUtils.refreshVectorDb(app);
+    OBDal.getInstance().refresh(app);
+    OpenAIUtils.syncAssistant(openaiApiKey, app);
+  }
+
+  /**
+   * This method synchronizes knowledge base files with the LangChain API for a given {@link CopilotApp}.
+   * It performs several steps to ensure that the application's vector database is properly updated
+   * and the knowledge base files are synchronized.
+   *
+   * <p>The synchronization process involves resetting the vector database, processing each
+   * {@link CopilotAppSource} in the list to synchronize it with the LangChain API, and then
+   * purging the vector database to remove any outdated information.
+   *
+   * @param app
+   *     The {@link CopilotApp} instance for which knowledge base files are being synchronized.
+   * @param knowledgeBaseFiles
+   *     A list of {@link CopilotAppSource} objects representing the knowledge base files to be synchronized.
+   * @throws JSONException
+   *     If an error occurs while processing JSON data.
+   * @throws IOException
+   *     If an input/output error occurs during the synchronization process.
+   */
+  private void syncKBFilesToLangChain(CopilotApp app, List<CopilotAppSource> knowledgeBaseFiles) throws JSONException, IOException {
+    CopilotUtils.resetVectorDB(app);
+    for (CopilotAppSource appSource : knowledgeBaseFiles) {
+      CopilotUtils.syncAppLangchainSource(appSource);
+    }
+    CopilotUtils.purgeVectorDB(app);
+  }
+
   private void syncOpenaiModels(String openaiApiKey) {
     //ask to openai for the list of models
     JSONArray modelJSONArray = OpenAIUtils.getModelList(openaiApiKey);
@@ -260,8 +360,8 @@ public class SyncOpenAIAssistant extends BaseProcessActionHandler {
       try {
         JSONObject modelObj = modelJSONArray.getJSONObject(i);
         if (!StringUtils.startsWith(modelObj.getString("id"), "ft:") && //exclude the models that start with gpt-4o
-            !StringUtils.equals(modelObj.getString("owned_by"), "openai-dev") &&
-            !StringUtils.equals(modelObj.getString("owned_by"), "openai-internal")) {
+                !StringUtils.equals(modelObj.getString("owned_by"), "openai-dev") &&
+                !StringUtils.equals(modelObj.getString("owned_by"), "openai-internal")) {
           modelIds.add(modelObj);
         }
       } catch (JSONException e) {
@@ -326,27 +426,20 @@ public class SyncOpenAIAssistant extends BaseProcessActionHandler {
     return model -> StringUtils.equals(model.optString("id"), modelInDB.getSearchkey());
   }
 
-  private int callSync(int syncCount, String openaiApiKey, CopilotApp app) {
-    OpenAIUtils.syncAssistant(openaiApiKey, app);
-    syncCount++;
-
-    return syncCount;
-  }
-
-  private void returnSuccessMsg(JSONObject result, int syncCount,
-      int totalRecords) throws JSONException {
-
+  private JSONObject buildMessage(int syncCount, int totalRecords) throws JSONException {
+    JSONObject result = new JSONObject();
     // Message in tab from where the process is executed
     JSONArray actions = new JSONArray();
     JSONObject showMsgInProcessView = new JSONObject();
     showMsgInProcessView.put("msgType", "success");
     showMsgInProcessView.put("msgTitle", OBMessageUtils.messageBD("Success"));
     showMsgInProcessView.put("msgText",
-        String.format(String.format(OBMessageUtils.messageBD("ETCOP_SuccessSync"), syncCount, totalRecords)));
+            String.format(String.format(OBMessageUtils.messageBD("ETCOP_SuccessSync"), syncCount, totalRecords)));
     showMsgInProcessView.put("wait", true);
     JSONObject showMsgInProcessViewAction = new JSONObject();
     showMsgInProcessViewAction.put("showMsgInProcessView", showMsgInProcessView);
     actions.put(showMsgInProcessViewAction);
     result.put("responseActions", actions);
+    return result;
   }
 }
