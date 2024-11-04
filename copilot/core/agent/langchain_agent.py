e59c5177f1bc587f6453d7a0666ef68feb64ebf4
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2024-11-04 10:42:43 -0300

    Feature ETP-321: Fix import of langchain chroma and fix error

--- copilot/core/agent/langchain_agent.py
+++ copilot/core/agent/langchain_agent.py
@@ -1,24 +1,27 @@
 import os
-from typing import Dict, Final, Union, Optional
+from typing import Dict, Final, Optional, Union
 
-from langchain.agents import AgentExecutor, AgentOutputParser, create_openai_functions_agent
+from langchain.agents import (
+    AgentExecutor,
+    AgentOutputParser,
+    create_openai_functions_agent,
+)
 from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.tools.retriever import create_retriever_tool
-from langchain_community.vectorstores import Chroma
+from langchain_chroma.vectorstores import Chroma
 from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.runnables import AddableDict
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from langsmith import traceable
 
-from .agent import AgentResponse, CopilotAgent
-from .agent import AssistantResponse
 from .. import utils
 from ..memory.memory_handler import MemoryHandler
 from ..schemas import QuestionSchema, ToolSchema
-from ..utils import get_full_question, copilot_debug
-from ..vectordb_utils import get_embedding, get_vector_db_path, get_chroma_settings
+from ..utils import get_full_question
+from ..vectordb_utils import get_chroma_settings, get_embedding, get_vector_db_path
+from .agent import AgentResponse, AssistantResponse, CopilotAgent
 
 SYSTEM_PROMPT_PLACEHOLDER = "{system_prompt}"
 
@@ -43,8 +46,15 @@ class LangchainAgent(CopilotAgent):
         self._memory = MemoryHandler()
 
     @traceable
-    def get_agent(self, provider: str, open_ai_model: str, tools: list[ToolSchema] = None, system_prompt: str = None,
-                  temperature: float = 1, kb_vectordb_id: Optional[str] = None):
+    def get_agent(
+        self,
+        provider: str,
+        open_ai_model: str,
+        tools: list[ToolSchema] = None,
+        system_prompt: str = None,
+        temperature: float = 1,
+        kb_vectordb_id: Optional[str] = None,
+    ):
         """Construct and return an agent from scratch, using LangChain Expression Language.
 
         Raises:
@@ -63,33 +73,43 @@ class LangchainAgent(CopilotAgent):
 
     @traceable
     def get_agent_executor(self, agent) -> AgentExecutor:
-        agent_exec = AgentExecutor(agent=agent, tools=self._configured_tools, verbose=True, log=True,
-                                   handle_parsing_errors=True, debug=True)
+        agent_exec = AgentExecutor(
+            agent=agent,
+            tools=self._configured_tools,
+            verbose=True,
+            log=True,
+            handle_parsing_errors=True,
+            debug=True,
+        )
         agent_exec.max_iterations = utils.read_optional_env_var_int("COPILOT_MAX_ITERATIONS", 100)
         max_exec_time = utils.read_optional_env_var_float("COPILOT_EXECUTION_TIMEOUT", 0)
         agent_exec.max_execution_time = None if max_exec_time == 0 else max_exec_time
         return agent_exec
 
     @traceable
-    def get_openai_agent(self, open_ai_model, tools, system_prompt, temperature=1,
-                         kb_vectordb_id: Optional[str] = None):
-
+    def get_openai_agent(
+        self, open_ai_model, tools, system_prompt, temperature=1, kb_vectordb_id: Optional[str] = None
+    ):
         _llm = ChatOpenAI(temperature=temperature, streaming=False, model_name=open_ai_model)
         _enabled_tools = self.get_functions(tools)
 
-        if (kb_vectordb_id is not None and os.path.exists(get_vector_db_path(kb_vectordb_id))
-                and os.listdir(get_vector_db_path(kb_vectordb_id))):
+        if (
+            kb_vectordb_id is not None
+            and os.path.exists(get_vector_db_path(kb_vectordb_id))
+            and os.listdir(get_vector_db_path(kb_vectordb_id))
+        ):
             db_path = get_vector_db_path(kb_vectordb_id)
-            db = Chroma(persist_directory=db_path, embedding_function=get_embedding(),
-                        client_settings=get_chroma_settings())
+            db = Chroma(
+                persist_directory=db_path,
+                embedding_function=get_embedding(),
+                client_settings=get_chroma_settings(),
+            )
             # check if the db is empty
-            res = db.get(where={}, limit=1)
+            res = db.get(limit=1)
             if len(res["ids"]) > 0:
                 retriever = db.as_retriever()
                 tool = create_retriever_tool(
-                    retriever,
-                    "KnowledgeBaseRetriever",
-                    "Search for documents in the knowledge base."
+                    retriever, "KnowledgeBaseRetriever", "Search for documents in the knowledge base."
                 )
                 _enabled_tools.append(tool)
                 self._configured_tools.append(tool)
@@ -111,11 +131,7 @@ class LangchainAgent(CopilotAgent):
                     MessagesPlaceholder(variable_name="messages"),
                 ]
             )
-            agent = (
-                    prompt
-                    | llm
-                    | OpenAIToolsAgentOutputParser()
-            )
+            agent = prompt | llm | OpenAIToolsAgentOutputParser()
         return agent
 
     @traceable
@@ -137,34 +153,44 @@ class LangchainAgent(CopilotAgent):
                 ("user", "{input}"),
             ]
         )
-        _llm = ChatGoogleGenerativeAI(temperature=temperature, model=open_ai_model,
-                                      convert_system_message_to_human=True)
-        llm = _llm.bind(
+        _llm = ChatGoogleGenerativeAI(
+            temperature=temperature, model=open_ai_model, convert_system_message_to_human=True
         )
+        llm = _llm.bind()
         agent = (
-                {
-                    "system_prompt": lambda x: x["system_prompt"],
-                    "input": lambda x: x["input"],
-                }
-                | prompt
-                | llm
-                | CustomOutputParser()
+            {
+                "system_prompt": lambda x: x["system_prompt"],
+                "input": lambda x: x["input"],
+            }
+            | prompt
+            | llm
+            | CustomOutputParser()
         )
         return agent
 
     @traceable
     def execute(self, question: QuestionSchema) -> AgentResponse:
         full_question = get_full_question(question)
-        agent = self.get_agent(question.provider, question.model, question.tools, question.system_prompt,
-                               question.temperature, question.kb_vectordb_id)
+        agent = self.get_agent(
+            question.provider,
+            question.model,
+            question.tools,
+            question.system_prompt,
+            question.temperature,
+            question.kb_vectordb_id,
+        )
         executor: Final[AgentExecutor] = self.get_agent_executor(agent)
         messages = self._memory.get_memory(question.history, full_question)
-        langchain_respose: Dict = executor.invoke({"system_prompt": question.system_prompt, "messages": messages})
+        langchain_respose: Dict = executor.invoke(
+            {"system_prompt": question.system_prompt, "messages": messages}
+        )
         output_answer = {"response": langchain_respose["output"]}
-        return AgentResponse(input=full_question, output=AssistantResponse(
-            response=output_answer["response"],
-            conversation_id=question.conversation_id
-        ))
+        return AgentResponse(
+            input=full_question,
+            output=AssistantResponse(
+                response=output_answer["response"], conversation_id=question.conversation_id
+            ),
+        )
 
     @traceable
     def get_tools(self):
@@ -172,31 +198,29 @@ class LangchainAgent(CopilotAgent):
 
     async def aexecute(self, question: QuestionSchema) -> AgentResponse:
         copilot_stream_debug = os.getenv("COPILOT_STREAM_DEBUG", "false").lower() == "true"  # Debug mode
-        agent = self.get_agent(question.provider, question.model, question.tools, question.system_prompt,
-                               question.temperature, question.kb_vectordb_id)
+        agent = self.get_agent(
+            question.provider,
+            question.model,
+            question.tools,
+            question.system_prompt,
+            question.temperature,
+            question.kb_vectordb_id,
+        )
         agent_executor: Final[AgentExecutor] = self.get_agent_executor(agent)
         full_question = question.question
         if question.local_file_ids is not None and len(question.local_file_ids) > 0:
             full_question += "\n\n" + "LOCAL FILES: " + "\n".join(question.local_file_ids)
         messages = self._memory.get_memory(question.history, full_question)
-        _input = {
-            "content": full_question,
-            "messages": messages,
-            "system_prompt": question.system_prompt
-        }
+        _input = {"content": full_question, "messages": messages, "system_prompt": question.system_prompt}
         if question.conversation_id is not None:
             _input["thread_id"] = question.conversation_id
         async for event in agent_executor.astream_events(_input, version="v2"):
             if copilot_stream_debug:
-                yield AssistantResponse(
-                    response=str(event), conversation_id='', role="debug"
-                )
+                yield AssistantResponse(response=str(event), conversation_id="", role="debug")
             kind = event["event"]
             if kind == "on_tool_start":
                 if len(event["parent_ids"]) == 1:
-                    yield AssistantResponse(
-                        response=event["name"], conversation_id='', role="tool"
-                    )
+                    yield AssistantResponse(response=event["name"], conversation_id="", role="tool")
             elif kind == "on_chain_end":
                 if not type(event["data"]["output"]) == AddableDict:
                     output = event["data"]["output"]
