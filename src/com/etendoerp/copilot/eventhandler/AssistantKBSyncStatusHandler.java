26b1e47b2c96555929f830292f5b77e23d41da95
Valentin Vivaldi <valentin.vivaldi@smfconsulting.es>
2024-12-10 09:34:04 -0300

    Feature ETP-660: Add validation to prevent orphan Etcop_appsource

--- src/com/etendoerp/copilot/eventhandler/AssistantKBSyncStatusHandler.java
+++ src/com/etendoerp/copilot/eventhandler/AssistantKBSyncStatusHandler.java
@@ -3,6 +3,7 @@ package com.etendoerp.copilot.eventhandler;
 import javax.enterprise.event.Observes;
 
 import org.apache.log4j.Logger;
+import org.openbravo.base.exception.OBException;
 import org.openbravo.base.model.Entity;
 import org.openbravo.base.model.ModelProvider;
 import org.openbravo.base.model.Property;
@@ -11,6 +12,8 @@ import org.openbravo.client.kernel.event.EntityNewEvent;
 import org.openbravo.client.kernel.event.EntityPersistenceEventObserver;
 import org.openbravo.client.kernel.event.EntityUpdateEvent;
 import org.openbravo.dal.service.OBDal;
+import org.openbravo.erpCommon.utility.OBMessageUtils;
+import org.openbravo.model.ad.module.Module;
 
 import com.etendoerp.copilot.data.CopilotApp;
 import com.etendoerp.copilot.data.CopilotAppSource;
@@ -44,26 +47,31 @@ public class AssistantKBSyncStatusHandler extends EntityPersistenceEventObserver
    * CopilotAppSource entity has been modified, it sets the synchronization status of
    * the associated CopilotApp to 'Pending Synchronization'.
    *
-   * @param event the entity update event to be observed
+   * @param event
+   *     the entity update event to be observed
    */
   public void onUpdate(@Observes EntityUpdateEvent event) {
     if (!isValidEvent(event)) {
       return;
     }
     final CopilotAppSource currentAppSource = (CopilotAppSource) event.getTargetInstance();
-    Object previousValue = event.getPreviousState(currentAppSource.getEntity().getProperty(CopilotAppSource.PROPERTY_FILE));
-    Object currentValue = event.getCurrentState(currentAppSource.getEntity().getProperty(CopilotAppSource.PROPERTY_FILE));
+    Object previousValue = event.getPreviousState(
+        currentAppSource.getEntity().getProperty(CopilotAppSource.PROPERTY_FILE));
+    Object currentValue = event.getCurrentState(
+        currentAppSource.getEntity().getProperty(CopilotAppSource.PROPERTY_FILE));
     if (!previousValue.equals(currentValue)) {
       changeAssistantStatus(currentAppSource);
       CopilotUtils.logIfDebug("The register was updated and the sync status changed to PS");
     }
+    validateModuleExport(currentAppSource);
   }
 
   /**
    * Handles the save event for CopilotAppSource entities. It sets the synchronization status
    * of the associated CopilotApp to 'Pending Synchronization'.
    *
-   * @param event the entity save event to be observed
+   * @param event
+   *     the entity save event to be observed
    */
   public void onSave(@Observes EntityNewEvent event) {
     if (!isValidEvent(event)) {
@@ -72,13 +80,15 @@ public class AssistantKBSyncStatusHandler extends EntityPersistenceEventObserver
     final CopilotAppSource currentAppSource = (CopilotAppSource) event.getTargetInstance();
     changeAssistantStatus(currentAppSource);
     CopilotUtils.logIfDebug("The register was saved and the sync status changed to PS");
+    validateModuleExport(currentAppSource);
   }
 
   /**
    * Handles the delete event for CopilotAppSource entities. It sets the synchronization status
    * of the associated CopilotApp to 'Pending Synchronization'.
    *
-   * @param event the entity delete event to be observed
+   * @param event
+   *     the entity delete event to be observed
    */
   public void onDelete(@Observes EntityDeleteEvent event) {
     if (!isValidEvent(event)) {
@@ -92,11 +102,35 @@ public class AssistantKBSyncStatusHandler extends EntityPersistenceEventObserver
   /**
    * Changes the synchronization status of the associated CopilotApp to 'Pending Synchronization'.
    *
-   * @param currentAppSource the CopilotAppSource entity whose associated CopilotApp's status needs to be updated
+   * @param currentAppSource
+   *     the CopilotAppSource entity whose associated CopilotApp's status needs to be updated
    */
   private static void changeAssistantStatus(CopilotAppSource currentAppSource) {
     CopilotApp currentAssistant = currentAppSource.getEtcopApp();
     currentAssistant.setSyncStatus(CopilotConstants.PENDING_SYNCHRONIZATION_STATE);
     OBDal.getInstance().save(currentAssistant);
+  }
+
+  /**
+   * Validates the module export for the given CopilotAppSource entity.
+   * <p>
+   * This method checks if the associated CopilotApp's module and the file's module are not null.
+   * If the file's module is null, it throws an OBException indicating that the file does not belong to any module.
+   *
+   * @param currentAppSource
+   *     the CopilotAppSource entity to validate
+   * @throws OBException
+   *     if the file's module is null
+   */
+  private void validateModuleExport(CopilotAppSource currentAppSource) {
+    Module assistantModule = currentAppSource.getEtcopApp().getModule();
+    if (assistantModule == null) {
+      return;
+    }
+    Module fileModule = currentAppSource.getFile().getModule();
+    if (fileModule == null) {
+      throw new OBException(
+          String.format(OBMessageUtils.messageBD("ETCOP_FileNotBelonging"), currentAppSource.getFile().getName()));
+    }
   }
 }
